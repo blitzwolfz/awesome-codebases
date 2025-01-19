2d112be9ff1ed5e013e2102ca8e94241081179e5
Sammy Q <saminm.qureshi@gmail.com>
2025-01-19 15:44:26 -0500

    Added lab2.py with coding solutions

---
+++ lab2.py
@@ -0,0 +1,107 @@
+import random
+
+def get_integer_input(prompt):
+    """Get an integer input from the user with error handling."""
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+# Define Variables
+numLives = 10           # number of player's lives remaining
+mNumLives = 12          # number of monster's lives remaining
+
+diceOptions = [1, 2, 3, 4, 5, 6]
+weapons = ["Fist", "Knife", "Club", "Gun", "Bomb", "Nuclear bomb"]
+
+combatStrength = get_integer_input("Enter your combat Strength: ")
+mCombatStrength = get_integer_input("Enter the monster's combat Strength: ")
+
+input("Roll the dice for your health points (Press enter)")
+healthPoints = random.choice(diceOptions)
+print("You rolled " + str(healthPoints) + " health points")
+
+input("Roll the dice for the monster's health points (Press enter)")
+mHealthPoints = random.choice(diceOptions)
+print("You rolled " + str(mHealthPoints) + " health points for the monster")
+
+input("Roll the dice to see if you find a healing potion (Press enter)")
+healingPotion = random.choice([0, 1])
+print("Have you found a healing potion?: " + str(bool(healingPotion)))
+
+# Weapon roll
+input("Roll the dice to choose your weapon (Press enter)")
+weaponRoll = random.choice(diceOptions) - 1  # Subtract 1 to match the array index
+selectedWeapon = weapons[weaponRoll]
+combatStrength += weaponRoll + 1  # Add weapon strength to combat strength
+
+print(f"You rolled a {weaponRoll + 1} and got the weapon: {selectedWeapon}")
+
+if weaponRoll + 1 <= 2:
+    print("You rolled a weak weapon, friend.")
+elif weaponRoll + 1 <= 4:
+    print("Your weapon is meh.")
+else:
+    print("Nice weapon, friend!")
+
+if selectedWeapon != "Fist":
+    print("Thank goodness you didn't roll the Fist...")
+
+# Analyze the roll
+input("Analyze the roll (Press enter)")
+# Equality operators
+print("--- You are matched in strength: " + str(combatStrength == mCombatStrength))
+
+# Relational operators
+print("--- You have a strong player: " + str((combatStrength + healthPoints) >= 15))
+
+# and keyword
+print("--- Remember to take a healing potion!: " + str(healingPotion == 1 and healthPoints <= 6))
+
+# not keyword
+print("--- Phew, you have a healing potion: " + str(
+    not (
+        healthPoints < mCombatStrength  # monster will NOT kill hero in one blow
+    )
+    and
+    healingPotion == 1                  # hero has a healing potion
+))
+
+# or keyword
+print("--- Things are getting dangerous: " + str(healingPotion == 0 or healthPoints == 1))
+
+# in keyword
+print("--- Is it possible to roll 0 in the dice?: " + str(0 in diceOptions))
+
+# --- Expanded if statement
+if healthPoints >= 5:
+    print("--- Your health is ok")
+elif healingPotion == 1:
+    healingPotion = 0
+    healthPoints = 6
+    print("--- Using your healing potion... Your Health Points is now full at " + str(healthPoints))
+else:
+    print("--- Your health is low at " + str(healthPoints) + " and you have no healing potions available!")
+
+# --- Nested if statement
+print("You meet the monster. FIGHT!!")
+input("You strike first (Press enter)")
+
+print("Your weapon (" + str(combatStrength) + ") ---> Monster (" + str(mHealthPoints) + ")")
+if combatStrength >= mHealthPoints:
+    mHealthPoints = 0
+    print("You've killed the monster")
+else:
+    mHealthPoints -= combatStrength
+
+    print("You've reduced the monster's health to: " + str(mHealthPoints))
+
+    print("The monster strikes!!!")
+    print("Monster's Claw (" + str(mCombatStrength) + ") ---> You (" + str(healthPoints) + ")")
+    if mCombatStrength >= healthPoints:
+        healthPoints = 0
+        print("You're dead")
+    else:
+        healthPoints -= mCombatStrength
+        print("The monster has reduced your health to: " + str(healthPoints))
